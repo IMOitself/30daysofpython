7ff69f1e58b1b0eb46ddb80d21531bc6620e4ab3
IMOitself <russjonn@gmail.com>
2024-09-10 22:39:51 +0800

    init >:D

---
+++ day 1/hi.py
@@ -0,0 +1,18 @@
+# day 1 hehe
+# using vim as well
+print(11 + 99)
+print(100 - 11)
+print(10 * 10)
+print(4 / 3)
+print(2 ** 3)
+print(3 % 10)
+print(4 // 3)
+
+print(type(10))
+print(type(0.5))
+print(type(3 + 3j))
+print(type('america ya:D'))
+print(type([1, 2, 3])) # list
+print(type({1, 2, 3})) # Set
+print(type((1, 2, 3))) # Tuple
+print(type({'america ya!':'hallo:D'})) # Dictionary
