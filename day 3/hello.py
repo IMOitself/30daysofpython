447a0594236edbda65763cd83e3dca2279596b30
IMOitself <russjonn@gmail.com>
2024-09-12 22:07:22 +0800

    day 3

---
+++ day 3/hello.py
@@ -0,0 +1,15 @@
+# not in the mood rn to code right after i watched tensura diaries
+age = 4
+print(age, type(age))
+
+height = 3.14
+print(height, type(height))
+
+complex_num = 1j
+print(complex_num, type(complex_num))
+
+base = int(input('base: ',))
+height = int(input('height: '))
+print('area of triangle is ', 0.5 * (base * height))
+
+
