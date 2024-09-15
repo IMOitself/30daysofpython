ebdb479789e126c29ca3f2a5eefb1d229a905150
IMOitself <russjonn@gmail.com>
2024-09-15 23:18:55 +0800

    day 4

---
+++ day 4/hi.py
@@ -0,0 +1,45 @@
+# String manipulation or smth
+
+fweind1 = 'chimken'
+fweind2 = 'DOGE'
+fweind3 = 'birb'
+fweind4 = 'frogge'
+
+print(f'my bff: {fweind1}')
+print(f'annoying ferson: {fweind2}')
+
+print('our team name is ' + fweind1 + fweind2 + fweind3 + fweind4)
+
+winged_fweinds = 2
+
+print('%d of my fweinds have wings, %s and %s' %(winged_fweinds, fweind1, fweind3))
+
+print("only {} has a paw and {} of my fweinds are jealous".format(fweind2, winged_fweinds))
+
+print(f"{fweind4} is annoying, he jumps a lot")
+
+eyebrow, eyes, mouth = '>:D'
+face = eyebrow + eyes + mouth
+
+print(eyebrow + eyes + mouth)
+print(eyebrow)
+print(eyes)
+print(mouth)
+print()
+
+print(f'{face} -> {face[1]}{face[2]}')
+
+eyebrow = ''
+eyes = ':'
+mouth = '('
+print(eyebrow + eyes + mouth)
+print(eyebrow)
+print(eyes)
+print(mouth)
+print()
+
+print(f'{fweind2} lowercase: {fweind2.lower()}')
+print(f'{fweind1} uppercase: {fweind1.upper()}')
+
+
+
